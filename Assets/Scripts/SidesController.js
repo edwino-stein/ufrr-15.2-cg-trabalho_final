22ffe2dcea272a555aeb562cfe216df84d43f1c1
Edwino Stein <edwino.stein@gmail.com>
2016-03-15 23:29:47 -0400

    Adicionando limitador nos lados

---
+++ Assets/Scripts/SidesController.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+//Espaço de folga
+var clearance:float;
+
+function OnTriggerEnter2D(col : Collider2D) {
+
+	//Se o jogador sair por um lado faz com que apareça no outro lado
+	if(col.tag == "Player"){
+		col.transform.position.x = col.transform.position.x*(-1) + (col.transform.position.x > 0 ? clearance : (-1)*clearance);
+	}
+}
