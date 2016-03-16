4779e388dca1653beddf6588024da04c5ea4673c
Edwino Stein <edwino.stein@gmail.com>
2016-03-15 23:13:43 -0400

    Adicionando serras

---
+++ Assets/Scripts/SawController.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+//Velocidade de rotaçao
+var spinVelocity:float;
+
+function Update () {
+	//Gira a serra
+	this.transform.Rotate(0, 0, Time.deltaTime*spinVelocity);
+}
+
+function OnTriggerEnter2D(col : Collider2D) {
+
+	//Detecta quando o jogador toca na serra
+	if(col.tag == "Player"){
+		//Desativa qualquer colisao do jogador
+		col.isTrigger = true;
+		
+		//Informa ao GameMaster a morte do jogador
+		GameObject.Find("GM").SendMessage("playerDied");
+	}
+}
+
+function OnTriggerExit2D(col : Collider2D){
+
+	//Detecta quando o jogador sai da area de colisao da serra
+	if(col.tag == "Player"){
+		//Informa a morte ao controlador do jogador
+		col.transform.SendMessage("playerDied");
+	}
+}
