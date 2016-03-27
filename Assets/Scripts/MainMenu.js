26be06e7604c9d21370115fcda5349b0719d5d99
Rodrigo Tavares <rodrigost23@gmail.com>
2016-03-27 19:45:56 -0400

    Botão de voltar no menu inicial fecha o jogo

--- Assets/Scripts/MainMenu.js
+++ Assets/Scripts/MainMenu.js
@@ -5,9 +5,15 @@ function Start () {
 }
 
 function Update () {
-
+	if(Input.GetKeyUp(KeyCode.Escape)){
+		QuitGame();
+	}
 }
 
 function StartGame() {
 	Application.LoadLevel("Main");
+}
+
+function QuitGame() {
+	Application.Quit();
 }
